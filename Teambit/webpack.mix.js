f8b0f0674e29109dd85364ab0a2e8f0983f4993f
Radu <Schirliuradu@gmail.com>
2019-06-10 15:37:04 +0200

    Basic configuration workflow with b-sync and scss folder structure

---
+++ Teambit/webpack.mix.js
@@ -0,0 +1,16 @@
+
+let mix = require('laravel-mix');
+
+mix.js('src/js/index.js', 'dist/js/script.js');
+mix.sass('src/scss/main.scss', 'dist/css/style.css');
+
+var browserSync = require("browser-sync").create();
+
+browserSync.init({
+    watch: true,
+    port: 3000,
+    server: {
+      baseDir: "./dist",
+      index: "index.html",
+    }
+});
